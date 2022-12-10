2d83417b271bb9d9e1c73d5e08cc909b771462cb
royce-mathew <royce1mathew@gmail.com>
2022-12-10 14:26:20 -0500

    Added TwoSumIV_BST - Java

--- Easy/TwoSumIV_BST/Solution.java
+++ Easy/TwoSumIV_BST/Solution.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.HashSet;
 
 class TreeNode {
 	int val;
@@ -13,29 +14,51 @@ class TreeNode {
 
 
 class Solution {	
-	public static boolean findTarget(TreeNode node, int k) {
-
-		/*
-		 * If value + right is above, try value + left
-		 * 
-		 */
-
-		ArrayList mainList = new ArrayList<>();
-		Stack mainStack = new Stack<>();
-		TreeNode tempNode = node;
+	public static boolean findTarget(TreeNode root, int k) {
+        ArrayList mainList = new ArrayList<>();
+		Stack <TreeNode> mainStack = new Stack<TreeNode>();
+		HashSet mainMap = new HashSet();
+		mainStack.push(root);
 
 
-		while (tempNode != null) {
-			if (tempNode.left) {
+		while (true) {
+			if (mainStack.size() == 0) {break;} 
+
+			TreeNode tempNode = mainStack.pop();
+			mainMap.add(k - tempNode.val);
+			mainList.add(tempNode.val);
+
+			if (tempNode.left != null) {
 				mainStack.push(tempNode.left);
 			}
-			if (tempNode.right) {
+
+			if (tempNode.right != null) {
 				mainStack.push(tempNode.right);
 			}
 		}
 
-		return true;
-	}
+        if (mainList.size() <= 1) {
+			return false;
+		}
+
+
+		for (int i = 0; i < mainList.size(); i++) {
+            int curr_val = (int) mainList.get(i);
+
+            if (mainMap.contains(curr_val)) {
+                if (k - curr_val == curr_val)  {
+                    if (!mainList.contains((int) mainList.remove(i))){
+                        continue;
+                    }
+                }
+                return true;
+            }
+		}
+
+		return false;
+    }
+
+
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(5);
